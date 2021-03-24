762d1bb2fb1ed8284c59510a4696d395dc8565e4
fenix31415 <dimkakirov43@mail.ru>
2021-03-24 16:15:40 +0300

    just play sound with delay

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 ﻿#include "SKSE/Trampoline.h"
 #include <xbyak\xbyak.h>
 
+#include <thread>
+#include <chrono>
+
 extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a_skse, SKSE::PluginInfo* a_info)
 {
 #ifndef NDEBUG
@@ -59,12 +62,14 @@ void add_trampoline(Xbyak::CodeGenerator* xbyakCode)
 	trampoline.write_branch<BRANCH_TYPE>(funcAddr + offset, (std::uintptr_t)result);
 }
 
-uint64_t* data_ptr;
 char* str;
+
+void sleep() {
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+}
 
 void apply_oncontunue()
 {
-	logger::info("apply_oncontunue");
 	const int ID = 51245, BRANCH_TYPE = 5;
 	constexpr REL::ID funcOffset = REL::ID(ID);
 	auto funcAddr = funcOffset.address();
@@ -72,69 +77,43 @@ void apply_oncontunue()
 	constexpr REL::ID subOffset(52014);
 	auto subAddr = subOffset.address();
 
+	constexpr REL::ID playSound_offset(52054);
+	auto playSound_addr = playSound_offset.address();
+
 	struct Code : Xbyak::CodeGenerator
 	{
-		Code(uintptr_t ret_addr, uintptr_t sub_addr)
+		Code(uintptr_t play_sound, uintptr_t ret_addr, uintptr_t sub_addr)
 		{
-			mov(rax, (uintptr_t)&data_ptr);
-			mov(rax, ptr[rax]);
-			mov(dword[rax], 0xfe314159);
+			push(rcx);
+			push(rcx);
+
+			mov(rcx, (uintptr_t)&str);
+			mov(rcx, ptr[rcx]);
+			mov(rax, play_sound);
+			call(rax);
+			mov(rax, (uintptr_t)sleep);
+			call(rax);
+
+			pop(rcx);
+			pop(rcx);
 			mov(rax, ret_addr);
 			push(rax);
 			mov(rax, sub_addr);
 			jmp(rax);
 		}
-	} xbyakCode{ funcAddr + 0x16 + BRANCH_TYPE, subAddr };
+	} xbyakCode{ playSound_addr, funcAddr + 0x16 + BRANCH_TYPE, subAddr };
 	add_trampoline<BRANCH_TYPE, ID, 0x16>(static_cast<Xbyak::CodeGenerator*>(&xbyakCode));
-}
-
-void apply_playsound()
-{
-	logger::info("apply_playsound");
-	const int ID = 52051, BRANCH_TYPE = 5;
-	constexpr REL::ID funcOffset = REL::ID(ID);
-
-	constexpr REL::ID subOffset(52054);
-	auto subAddr = subOffset.address();
-
-	logger::info("apply_playsound1");
-
-	struct Code : Xbyak::CodeGenerator
-	{
-		Code(uintptr_t sub_addr)
-		{
-			Xbyak::Label jump;
-
-			mov(rax, (uintptr_t)&data_ptr);
-			mov(rax, ptr[rax]);
-			cmp(dword[rax], 0xfe314159);
-			jne(jump);
-			mov(dword[rax], 0x0);
-			mov(rax, (uintptr_t)&str);
-			mov(rcx, ptr[rax]);
-
-			L(jump);
-			mov(rax, sub_addr);
-			jmp(rax);
-		}
-	} xbyakCode{ subAddr };
-	logger::info("apply_playsound, add_trampoline");
-	add_trampoline<BRANCH_TYPE, ID, 0x1B>(static_cast<Xbyak::CodeGenerator*>(&xbyakCode));
-	logger::info("apply_playsound, add_trampoline OK");
 }
 
 void apply_hooks()
 {
-	logger::info("apply_hooks");
 	SKSE::AllocTrampoline(1 << 10);
 
-	data_ptr = new uint64_t(0);
 	const char s[15] = "UIStartNewGame";
 	constexpr size_t s_len = sizeof(s);
 	str = new char[s_len];
 	memcpy(str, s, s_len);
 
-	apply_playsound();
 	apply_oncontunue();
 }
 
